96d43c2eca10aed91787e564dbd932c8a8ddd13d
seiriosalpha <seiriosalpha@gmail.com>
2022-08-30 16:36:08 +0800

    Update test

---
+++ config.py
@@ -0,0 +1,10 @@
+#####        Environment Settings       #####
+
+# masterkeyring:
+#  driver: keyring
+#  service: system
+#
+# key_pass: zaCELgL0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx
+
+# master_sign_pubkey: True
+# signing_key_pass: 
